fa787d0333077bbc55193cc8cef81a6cde40bf8d
Udit Singh <uditsinghsfc@gmail.com>
2024-12-06 11:18:15 +0530

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="sticky w-full footer left-0 bottom-0 flex justify-center items-center mb-2 linen">
+     
+      <div className="font-semibold ">Copyright&copy; 2024 Udit Singh</div>
+    </footer>
+  );
+}
+
+export default Footer;
